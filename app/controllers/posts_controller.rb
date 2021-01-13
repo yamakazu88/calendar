196ba43129a209b1d36c19a67d9a8c24ce5295c9
yamakazu88 <yamakazu.9683@gmail.com>
2021-01-13 15:18:57 +0900

    postコントローラーのリファクタリング１

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :search_post, only: [:index, :search]
+  before_action :post_set, only: [:show, :destroy, :edit, :update]
 
   def index
     @posts = Post.where(user_id: current_user).order("start_time DESC").page(params[:page]).per(5)
@@ -19,24 +20,20 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path, notice:"削除しました"
   end
 
   def edit
-    @post = Post.find(params[:id])
     if current_user.id != @post.user_id
       redirect_to root_path
     end
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to root_path, notice: "編集しました"
     else
@@ -53,6 +50,10 @@ class PostsController < ApplicationController
   end
 
   private
+
+  def post_set
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:start_time, :title, :reason, :memo, :category_id).merge(user_id: current_user.id)
